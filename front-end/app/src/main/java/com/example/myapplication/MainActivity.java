6e47c1ff6aab0c26de11e9cec1222b66899a1826
Yingqi <luoyingqifr@gmail.com>
2023-02-19 22:26:18 +0100

    Add widgets in app

--- front-end/app/src/main/java/com/example/myapplication/MainActivity.java
+++ front-end/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Icon;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.lang.reflect.Modifier;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView musicTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        musicTitle = findViewById(R.id.music_title);
+        musicTitle.setSelected(true);
+
+
     }
 }
