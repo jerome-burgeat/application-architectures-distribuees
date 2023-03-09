f9ae917da993f9720a4a36df64ac8cc5aa923810
Yingqi <luoyingqifr@gmail.com>
2023-03-09 22:17:34 +0100

    Add component timestart and timerest

--- front-end/app/src/main/java/com/example/myapplication/MainActivity.java
+++ front-end/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,20 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.drawable.Icon;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import java.lang.reflect.Modifier;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView musicTitle;
+    ImageView musicImage;
+    TextView musicTitle, musicTimePass, musicTimeRest;
+    SeekBar musicSeekBar;
+    Button buttonPlay, buttonPrevious, buttonNext, buttonSppeech;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        musicImage = findViewById(R.id.music_image);
         musicTitle = findViewById(R.id.music_title);
+        musicTimePass = findViewById(R.id.music_time_pass);
+        musicTimeRest = findViewById(R.id.music_time_rest);
+        musicSeekBar = findViewById(R.id.music_seekBar);
+        buttonPlay = findViewById(R.id.button_play);
+        buttonPrevious = findViewById(R.id.button_previous);
+        buttonNext = findViewById(R.id.button_next);
+        buttonSppeech = findViewById(R.id.button_speech);
+
         musicTitle.setSelected(true);
 
 
