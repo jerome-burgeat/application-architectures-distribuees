2ef1765d2f8e05d2b8769d63e55ffb2281a3f468
Jérôme Burgeat <burgeat84@hotmail.fr>
2023-04-02 15:22:34 +0200

    Feat(ASR) : import de l'ASR par SpeechBrain

---
+++ asr/main.py
@@ -0,0 +1,26 @@
+import glob
+import os
+import os.path
+import sys
+
+from speechbrain.pretrained import SpeakerRecognition
+from speechbrain.pretrained import EncoderDecoderASR
+
+sys.path.append(os.getcwd())
+
+authentification = SpeakerRecognition.from_hparams(source="speechbrain/spkrec-ecapa-voxceleb", savedir="pretrained_models/spkrec-ecapa-voxceleb")
+authentifier = glob.glob("/\\voixAutorise\\*.wav")
+
+decoderVoix = EncoderDecoderASR.from_hparams(source="speechbrain/asr-crdnn-commonvoice-fr", savedir="pretrained_models/asr-crdnn-commonvoice-fr")
+auth = False
+
+print(decoderVoix.transcribe_file(os.getcwd() + "\\voixAutorise\\testJerome.wav"))
+
+#for x in authentifier :
+#    score, prediction = authentification.verify_files("C:\\Users\\burge\\PycharmProjects\\asr\\new_audio.wav", x)
+#    if(prediction):
+#        auth = True
+#        decoderVoix.transcribe_file("C:\\Users\\burge\\PycharmProjects\\asr\\new_audio.wav")
+#        print(decoderVoix.transcribe_file("C:\\Users\\burge\\PycharmProjects\\asr\\voixAutorise\\testJerome.wav"))
+#        print(auth, x)
+#        break
